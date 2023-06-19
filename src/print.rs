6a7824065de42d82e262a7bfef1ce573421a6e56
jhleao <>
2023-06-19 20:51:53 -0300

    feat: add terminal width aware divider for better visualization

--- src/print.rs
+++ src/print.rs
@@ -1,12 +1,20 @@
 use chrono::Utc;
 use hyper::body::Bytes;
 use hyper::http::request::Parts;
+use termsize::Size;
+
+const DIVIDER_CHAR: &str = "=";
 
 /// Formats and the HTTP request to stdout with a timestamp header.
 pub fn print_http(parts: &Parts, body_bytes: &Bytes) {
     let body_str = String::from_utf8(body_bytes.to_vec()).unwrap();
 
     let date_iso = Utc::now().to_rfc2822();
+    let term_size = termsize::get().unwrap_or(Size { cols: 0, rows: 0 });
+    let term_width: usize = term_size.cols.into();
+    let date_len: usize = date_iso.len();
+    let divider_chunk = DIVIDER_CHAR.repeat((term_width - date_len - 2) / 2);
+    let divider = format!("\n{divider_chunk}[{date_iso}]{divider_chunk}");
 
     let mut headers_str = String::from("\n");
 
@@ -22,7 +30,7 @@ pub fn print_http(parts: &Parts, body_bytes: &Bytes) {
         }
     }
 
-    println!("{date_iso}",);
+    println!("{divider}",);
     println!("{} {} {:?}", parts.method, parts.uri, parts.version);
     println!("{}", headers_str);
     println!("\n{}", body_str);
